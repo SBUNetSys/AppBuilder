e5e4325ffc4ebdca1309d78e877af346f11a8276
Arron Cao <qqcao.cs@gmail.com>
2016-11-26 03:42:51 -0500

    add: app building

--- src/main/java/edu/stonybrook/cs/netsys/appbuilder/AppBuilderMain.java
+++ src/main/java/edu/stonybrook/cs/netsys/appbuilder/AppBuilderMain.java
@@ -4,21 +4,36 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Map;
 
 import edu.stonybrook.cs.netsys.appbuilder.data.App;
 
 import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.ACTIVITY_NAME;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.APP_NAME_TEXT;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.APP_TAG_IN_TEMPLATE;
 import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.BUILD_GRADLE_NAME;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.CODE_PATH;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.CONFIG_DIR_NAME;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.IC_LAUNCHER;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.LAUNCHER_ICON_PATH;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.LAYOUT_PATH;
 import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.MANIFEST_NAME;
-import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.STRINGS_NAME;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.MANIFEST_PATH;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.OUTPUT_DIR_NAME;
 import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.TEMPLATE_FOLDER_NAME;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.UIWEAR_ACTIVITY_LAYOUT_PATH;
+import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.UIWEAR_DEFAULT_ICON_PATH;
 
 /**
  * Created by qqcao on 11/24/16 Thursday. <p> Wear app building process: <p>
@@ -48,50 +63,103 @@ import static edu.stonybrook.cs.netsys.appbuilder.data.Constants.TEMPLATE_FOLDER
  * section also indexes item view ids and relate them to their parent, i.e., list-like views.
  */
 public class AppBuilderMain {
-    private static final String[] TEMPLATES = new String[]{MANIFEST_NAME, BUILD_GRADLE_NAME,
-            ACTIVITY_NAME, STRINGS_NAME};
-
-    private static String templateLocation;
-
     private static Configuration configuration;
+    private static final String unzippedFileDest = "build/temp/";
 
     static {
-        new AppBuilderMain();
         try {
-            configuration = FreeMarker.initialize(new File(templateLocation));
+            configuration = FreeMarker.initialize(new File(TEMPLATE_FOLDER_NAME));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private AppBuilderMain() {
-        URL url = getClass().getClassLoader().getResource(TEMPLATE_FOLDER_NAME);
-        assert url != null;
-        templateLocation = url.getFile();
-        System.out.println(templateLocation);
-    }
-
     public static void main(String[] args) throws IOException, TemplateException {
         // step 0, open socket listening for new wear app building request
         // step 1, new thread handle received file, pkgName.zip, unzip and put to temp folder
+        String receivedFileSavedPath = "src/main/resources/";
+        String fileName = "com.spotify.music.zip";
+        String pkgName = FilenameUtils.getBaseName(fileName);
+        System.out.println(pkgName);
+        String appOutPath = receivedFileSavedPath + pkgName + File.separator + OUTPUT_DIR_NAME;
+        System.out.println(appOutPath);
+        // extract zip file to temp folder
+        try {
+            ZipFile zipFile = new ZipFile(receivedFileSavedPath + fileName);
+            zipFile.extractAll(unzippedFileDest);
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+
+        String appNameFilePath = Paths.get(unzippedFileDest, pkgName,
+                CONFIG_DIR_NAME, APP_NAME_TEXT).toString();
+
+        String appName = FileUtils.readFileToString(new File(appNameFilePath),
+                Charset.defaultCharset());
+        System.out.println(appName);
         // step 2, start parsing below code starts here
 
         // generate project files based on templates
-        Map<String, Object> root = new HashMap<>();
+        HashMap<String, Object> root = new HashMap<>();
         App wearApp = new App();
-        wearApp.setAppName("Spotify");
-        wearApp.setAppPkgName("com.spotify.music");
-        root.put("app", wearApp);
+        wearApp.setAppName(appName);
+        wearApp.setAppPkgName(pkgName);
+        root.put(APP_TAG_IN_TEMPLATE, wearApp);
 
-        for (String templateName : TEMPLATES) {
-            Template temp = configuration.getTemplate(templateName);
-            Writer out = new OutputStreamWriter(System.out);
-            temp.process(root, out);
+//        // just for test
+//        for (String templateName : TEMPLATES) {
+//            Template temp = configuration.getTemplate(templateName);
+//            Writer out = new OutputStreamWriter(System.out);
+//            temp.process(root, out);
+//        }
+
+        // process build.gradle
+        Template buildGradleTemplate = configuration.getTemplate(BUILD_GRADLE_NAME);
+        File buildGradleFile = new File(appOutPath, FilenameUtils.getBaseName(BUILD_GRADLE_NAME));
+        FileUtils.touch(buildGradleFile);
+        buildGradleTemplate.process(root, new FileWriter(buildGradleFile));
+
+        // process MainActivity
+        Template mainActivityTemplate = configuration.getTemplate(ACTIVITY_NAME);
+        File mainActivityFile = new File(appOutPath + CODE_PATH + pkgName.replace(".", "/"),
+                FilenameUtils.getBaseName(ACTIVITY_NAME));
+        FileUtils.touch(mainActivityFile);
+        mainActivityTemplate.process(root, new FileWriter(mainActivityFile));
+
+        // process AndroidManifest.xml
+        Template manifestTemplate = configuration.getTemplate(MANIFEST_NAME);
+        File manifestFile = new File(appOutPath + MANIFEST_PATH,
+                FilenameUtils.getBaseName(MANIFEST_NAME));
+        FileUtils.touch(manifestFile);
+        manifestTemplate.process(root, new FileWriter(manifestFile));
+
+
+        // process launcher icon and activity_main.xml
+        File launcherIconFile = new File(Paths.get(unzippedFileDest, pkgName,
+                CONFIG_DIR_NAME, IC_LAUNCHER).toString());
+        File launcherIconDestFile = new File(appOutPath + LAUNCHER_ICON_PATH + IC_LAUNCHER);
+        if (launcherIconFile.exists()) {
+            FileUtils.copyFile(launcherIconFile, launcherIconDestFile);
+        } else {
+            //copy default icon
+            FileUtils.copyFile(new File(UIWEAR_DEFAULT_ICON_PATH), launcherIconDestFile);
         }
 
-        // pre-process mapping rules for generating xml layout
-        // TODO: 11/25/16 parse card layout, skip here
+        File mainLayoutFile = new File(UIWEAR_ACTIVITY_LAYOUT_PATH);
+        File mainLayoutDestFile = new File(appOutPath + LAYOUT_PATH );
+        FileUtils.copyFileToDirectory(mainLayoutFile, mainLayoutDestFile);
 
-        //
+        /*** below code relies on parsing both mapping rules and layout xml files ***/
+        // pre-process mapping rules for generating xml layout like card type
+
+
+        // process mapping rules and layout xml to set string values and drawable resources
+
+        // process mapping rules and layout xml to generate arrays.xml files
+
+        // copy above processed xml files to corresponding directory
+
+        // put all together, copy app project to WearAppEnv folder to start building apk
     }
+
 }
--- src/main/java/edu/stonybrook/cs/netsys/appbuilder/data/Constants.java
+++ src/main/java/edu/stonybrook/cs/netsys/appbuilder/data/Constants.java
@@ -6,7 +6,8 @@ package edu.stonybrook.cs.netsys.appbuilder.data;
  * Constants for AppBuilder
  */
 public class Constants {
-    public static final String TEMPLATE_FOLDER_NAME = "uiwear/template";
+    public static final String SRC_PATH = "src/main/resources/";
+    public static final String TEMPLATE_FOLDER_NAME = SRC_PATH + "uiwear/template";
     public static final String MANIFEST_NAME = "AndroidManifest.xml.ftl";
     public static final String BUILD_GRADLE_NAME = "build.gradle.ftl";
     public static final String ACTIVITY_NAME = "MainActivity.java.ftl";
@@ -16,8 +17,21 @@ public class Constants {
     public static final String RULE_DIR_NAME = "MappingRules";
     public static final String RES_DIR_NAME = "Resources";
     public static final String CONFIG_DIR_NAME = "Configs";
+    public static final String APP_NAME_TEXT = "app_name.txt";
+    public static final String IC_LAUNCHER = "ic_launcher.png";
+    public static final String OUTPUT_DIR_NAME = "AppOutput";
+    public static final String MANIFEST_PATH = "/src/main/";
+    public static final String CODE_PATH = "/src/main/java/";
+    public static final String DRAWABLE_PATH = "/src/main/res/drawable/";
+    public static final String LAYOUT_PATH = "/src/main/res/layout/";
+    public static final String VALUES_PATH = "/src/main/res/values/";
+    public static final String LAUNCHER_ICON_PATH = "/src/main/res/mipmap-hdpi/";
+    public static final String UIWEAR_DEFAULT_ICON_PATH = SRC_PATH + "uiwear/res/ic_launcher.png";
+    public static final String UIWEAR_ACTIVITY_LAYOUT_PATH = SRC_PATH + "uiwear/res/activity_main.xml";
 
     public static final String PREF_FILE_SUFFIX = "_pref";
     public static final String RULE_FILE_SUFFIX = "_rule";
     public static final String LAYOUT_FILE_SUFFIX = "_layout";
+
+    public static final String APP_TAG_IN_TEMPLATE = "app";
 }
